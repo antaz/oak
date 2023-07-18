9173c9dce09ce0a1d67b68c2e4f9af9499b46aec
Antar Azri <azantar@proton.me>
2023-07-18 22:29:49 +0100

    Reformat

--- lib/oak/cli.rb
+++ lib/oak/cli.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require "thor"
 require "oak/irc"
 
 module Oak
-    class CLI < Thor
-        desc "connect", "conects to libera server by default"
-        def connect
-            irc = IRC.new
-            irc.loop
-        end
+  # Command line interface
+  class CLI < Thor
+    desc "connect", "conects to libera server by default"
+    def connect
+      irc = IRC.new
+      irc.loop
     end
+  end
 end
--- lib/oak/irc.rb
+++ lib/oak/irc.rb
@@ -1,43 +1,50 @@
+# frozen_string_literal: true
+
 require "socket"
 
 module Oak
-    class IRC
-        def(initialize host='irc.libera.chat', port=6667)
-            @sock = TCPSocket.new host, port
-        end
-
-        def loop
-            nick 'oak'
-            user 'oak', 'Oak bot'
-            while line = @sock.gets
-                case line
-                when /PING/
-                    pong
-                when /376/
-                    join ["##testchannel"]
-                end
-                puts line
-            end
-        end
-
-        def pong
-            @sock.puts 'PONG'
-        end
-
-        def nick(nickname)
-            @sock.puts "NICK #{nickname}"
-        end
-
-        def user(name, realname)
-            @sock.puts "USER #{name} 0 * #{realname}"
-        end
-
-        def join(chans)
-            @sock.puts "JOIN #{chans.join ' '}"
-        end
-
-        def close
-            @sock.close
-        end
+  # IRC specification implementation
+  class IRC
+    def initialize(host = "irc.libera.chat", port = 6667)
+      @sock = TCPSocket.new host, port
     end
+
+    def loop
+      ident
+      while (line = @sock.gets)
+        case line
+        when /PING/
+          pong
+        when /376/
+          join ["##testchannel"]
+        end
+        puts line
+      end
+    end
+
+    def pong
+      @sock.puts "PONG"
+    end
+
+    def nick(nickname)
+      @sock.puts "NICK #{nickname}"
+    end
+
+    def user(name, realname)
+      @sock.puts "USER #{name} 0 * #{realname}"
+    end
+
+    def ident
+      nick "oak123"
+      user "oak123", "Oak bot"
+    end
+
+    def join(chans)
+      @sock.puts "JOIN #{chans.join " "}"
+    end
+
+    def close
+      @sock.close
+    end
+  end
 end
