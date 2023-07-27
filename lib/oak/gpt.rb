97093d5e75d98a4e32d88f7707218ed1b916b03d
Antar Azri <azantar@proton.me>
2023-07-27 15:27:05 +0100

    Hash names and send them in request

--- lib/oak/gpt.rb
+++ lib/oak/gpt.rb
@@ -9,21 +9,24 @@ module Oak
   module Gpt
     API = "https://api.openai.com/v1"
     TOKEN = ENV["OPENAI_TOKEN"]
-    @HEADER = { "Content-Type": "application/json", "Authorization": "Bearer #{TOKEN}" }
+    @header = {"Content-Type": "application/json", Authorization: "Bearer #{TOKEN}"}
     @context = []
 
     def self.completion(name, prompt)
+      name = Digest::MD5.hexdigest name
       uri = URI.parse "#{API}/chat/completions"
-      @context.push({ "role": "user", "name": name, "content": prompt })
+      @context.push({role: "user", name: name, content: prompt})
       res = Net::HTTP.start(uri.host, use_ssl: true) do |http|
-        req = Net::HTTP::Post.new uri, @HEADER
-        req.body = { "model": "gpt-3.5-turbo", "messages": @context }.to_json
+        req = Net::HTTP::Post.new uri, @header
+        req.body = {model: "gpt-3.5-turbo", messages: @context, max_tokens: 2000, temperature: 0.2}.to_json
         http.request req
       end
       case res.code
       when "200"
         @context.push(JSON.parse(res.body)["choices"][0]["message"])
-        @contex.shift if @context.length >= 10
+        if @context.length >= 10
+          @context.shift
+        end
         JSON.parse(res.body)["choices"][0]["message"]["content"]
       else
         "Error: #{res.message}"
@@ -33,8 +36,8 @@ module Oak
     def self.image(prompt)
       uri = URI.parse "#{API}/images/generations"
       res = Net::HTTP.start(uri.host, use_ssl: true) do |http|
-        req = Net::HTTP::Post.new uri, @HEADER
-        req.body = { "prompt": prompt, "n": 2, "size": "512x512" }.to_json
+        req = Net::HTTP::Post.new uri, @header
+        req.body = {prompt: prompt, n: 2, size: "512x512"}.to_json
         http.request req
       end
       case res.code
