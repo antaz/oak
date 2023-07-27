42cdb6511acba883abcdb0c70d2e956158319857
Antar Azri <azantar@proton.me>
2023-07-27 15:28:16 +0100

    Truncate long messages into chunks
    IRC puts a limit of 512 bytes per message

---
+++ lib/oak/trunc.rb
@@ -0,0 +1,14 @@
+module IIRC
+  module Truncate
+    module Verbs
+      def msg(target, msg)
+        msg.to_s.gsub(/(.{1,400})(?=\s|$)/, '\1' + "\n").lines do |line|
+          super(target, line)
+        end
+      end
+      alias_method :say, :msg
+    end
+
+    include Verbs
+  end
+end
