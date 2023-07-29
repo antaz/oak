516b1dce0046f947af3683ab630c4f8fdda05a17
Antar Azri <azantar@proton.me>
2023-07-29 21:22:48 +0100

    Add weather module

---
+++ lib/oak/weather.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "iirc"
+require "net/http"
+require "json"
+
+module Oak
+  module Weather
+    API = "http://api.openweathermap.org/data/2.5/weather"
+
+    def configure_weather
+      on :privmsg, :do_weather
+    end
+
+    def do_weather evt
+      case evt.message
+      when /^\.weather (.*)/
+        say weather $1
+      when /^\.weather/
+        say ".weather [city]"
+      end
+    end
+
+    private
+
+    def weather query
+      uri = URI(API)
+      params = {q: query, units: "metric", APPID: ENV["OWM_TOKEN"]}
+      uri.query = URI.encode_www_form(params)
+
+      res = Net::HTTP.get_response(uri)
+      weather = JSON.parse(res.body)
+      "#{weather["name"]}, #{weather["main"]["temp"]} °C" if res.code == "200"
+    end
+  end
+end
