1ca2529221948e54b4aca6b18b5178c0bd65bd60
Antar Azri <azantar@proton.me>
2023-07-29 21:15:19 +0100

    Clean up GPT module

--- lib/oak/gpt.rb
+++ lib/oak/gpt.rb
@@ -3,46 +3,50 @@
 require "net/http"
 require "uri"
 require "json"
+require "iirc"
 
 module Oak
   # ChatGPT API
   module Gpt
     API = "https://api.openai.com/v1"
-    TOKEN = ENV["OPENAI_TOKEN"]
-    @header = {"Content-Type": "application/json", Authorization: "Bearer #{TOKEN}"}
-    @context = []
 
-    def self.completion(name, prompt)
-      name = Digest::MD5.hexdigest name
-      uri = URI.parse "#{API}/chat/completions"
-      @context.push({role: "user", name: name, content: prompt})
-      res = Net::HTTP.start(uri.host, use_ssl: true) do |http|
-        req = Net::HTTP::Post.new uri, @header
-        req.body = {model: "gpt-3.5-turbo", messages: @context, max_tokens: 2000, temperature: 0.2}.to_json
-        http.request req
-      end
-      case res.code
-      when "200"
-        @context.push(JSON.parse(res.body)["choices"][0]["message"])
-        if @context.length >= 10
-          @context.shift
-        end
-        JSON.parse(res.body)["choices"][0]["message"]["content"]
-      else
-        "Error: #{res.message}"
+    def configure_completion
+      on :privmsg, :do_completion
+    end
+
+    def do_completion evt
+      case evt.message
+      when /^\.gpt (.*)/
+        prompt = ::Regexp.last_match(1)
+        say completion(evt.nick, prompt)
+      when /^\.gpt/
+        say ".gpt [prompt]"
       end
     end
 
-    def self.image(prompt)
-      uri = URI.parse "#{API}/images/generations"
+    private
+
+    def context
+      @context ||= []
+    end
+
+    def completion(name, prompt)
+      header = {"Content-Type": "application/json", Authorization: "Bearer #{ENV["OPENAI_TOKEN"]}"}
+      name = Digest::MD5.hexdigest name
+      uri = URI.parse "#{API}/chat/completions"
+      context.push({role: "user", name: name, content: prompt})
       res = Net::HTTP.start(uri.host, use_ssl: true) do |http|
-        req = Net::HTTP::Post.new uri, @header
-        req.body = {prompt: prompt, n: 2, size: "512x512"}.to_json
+        req = Net::HTTP::Post.new uri, header
+        req.body = {model: "gpt-3.5-turbo", messages: context, max_tokens: 2000, temperature: 0.2}.to_json
         http.request req
       end
       case res.code
       when "200"
-        JSON.parse(res.body)["data"][0]["url"]
+        context.push(JSON.parse(res.body)["choices"][0]["message"])
+        if context.length >= 4
+          context.shift
+        end
+        JSON.parse(res.body)["choices"][0]["message"]["content"]
       else
         "Error: #{res.message}"
       end
