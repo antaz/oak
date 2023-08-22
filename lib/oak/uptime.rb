d89e695769030e17b9f8c55e56fceb3554aa3215
Antar Azri <azantar@proton.me>
2023-08-23 00:16:14 +0100

    Fix formatting

--- lib/oak/uptime.rb
+++ lib/oak/uptime.rb
@@ -20,20 +20,20 @@ module Oak
     private
 
     def uptime
-        secs = (Time.now - TIME).to_i
-        mins = secs / 60
-        hours = mins / 60
-        days = hours / 24
+      secs = (Time.now - TIME).to_i
+      mins = secs / 60
+      hours = mins / 60
+      days = hours / 24
 
-        if days > 0
-            "#{days} days and #{hours % 24} hours"
-        elsif hours > 0
-            "#{hours} hours and #{mins % 60} minutes"
-        elsif mins > 0
-            "#{mins} minutes and #{secs % 60} seconds"
-        elsif secs >= 0
-            "#{secs} seconds"
-        end 
+      if days > 0
+        "#{days} days and #{hours % 24} hours"
+      elsif hours > 0
+        "#{hours} hours and #{mins % 60} minutes"
+      elsif mins > 0
+        "#{mins} minutes and #{secs % 60} seconds"
+      elsif secs >= 0
+        "#{secs} seconds"
+      end
     end
   end
 end
